7ea594f420b02d504c10a0f437c31b46183e5138
bio-jung <pmj85719@naver.com>
2025-04-21 12:32:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ audience = [120, 135, 160, 155, 110, 90, 100, 120, 140, 150]
 
 # 그래프 그리기
 plt.figure(figsize=(10, 6))
-plt.plot(years, audience, marker='o', linestyle='-', linewidth=2)
-plt.title('2000~2025년 클래식 음악 관람객 변화', fontsize=16)
+plt.bar(years, audience)
+plt.title('2000~2025년 클래식 음악 관람객 변화 (바 차트)', fontsize=16)
 plt.xlabel('연도', fontsize=14)
 plt.ylabel('관람객 수 (만 명)', fontsize=14)
-plt.grid(True)
+plt.grid(axis='y')  # y축에만 그리드 표시
 plt.xticks(years, rotation=45)
 plt.tight_layout()
 
 # 그래프 보여주기
 plt.show()
+
